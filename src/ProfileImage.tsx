8b2fb0c777f3b7f61d42cda3cf2710398441feba
abarolli <obarolli@gmail.com>
2025-01-30 13:07:30 -0700

    Added ProfileImage styling

--- src/ProfileImage.tsx
+++ src/ProfileImage.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-function ProfileImage() {
-  return <img src="/me.jpeg" />;
+import styles from "./ProfileImage.module.css";
+
+interface Props {
+  src: string;
+  width: string;
+}
+
+function ProfileImage({ src, width }: Props) {
+  return (
+    <div
+      style={{ width, height: width, backgroundImage: `url(${src})` }}
+      className={styles.imageContainer}
+    />
+  );
 }
 
 export default ProfileImage;
