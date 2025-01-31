f26da561cdadb7d018997aadbb81c67e8cb01db8
abarolli <obarolli@gmail.com>
2025-01-31 10:12:25 -0700

    Updated Screen.tsx

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,15 +1,15 @@
-import React, { CSSProperties, ReactNode } from "react";
+import React from "react";
 
 import styles from "./Screen.module.css";
 
 interface Props {
-  children: ReactNode;
-  style?: CSSProperties;
+  children: React.ReactNode;
+  style?: React.CSSProperties;
 }
 
 function Screen({ children, style }: Props) {
   return (
-    <div style={style} className={styles.screen}>
+    <div className={styles.screen} style={style}>
       {children}
     </div>
   );
