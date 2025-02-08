83ee29211db16013438fbcef5c7ae3b0251c371b
abarolli <obarolli@gmail.com>
2025-02-08 16:34:23 -0700

    Refactored p styles

--- src/components/WelcomeScreenSummary.tsx
+++ src/components/WelcomeScreenSummary.tsx
@@ -4,7 +4,7 @@ import colors from "../configs/colors";
 function WelcomeScreenSummary() {
   return (
     <div style={{ whiteSpace: "wrap" }}>
-      <p style={{ marginBottom: "20px", color: colors.text }}>
+      <p style={{ marginBottom: "20px" }}>
         My name's Adeiron (Oni) Barolli and I am a passionate software developer
         with expertise in technologies and languages such as{" "}
         <span style={{ color: colors.brightNeonGreen }}>Java</span>,{" "}
@@ -13,14 +13,14 @@ function WelcomeScreenSummary() {
         <span style={{ color: colors.electricCyan }}>SQL</span>,{" "}
         <span style={{ color: colors.cyberLime }}>Django</span>, and more.
       </p>
-      <p style={{ marginBottom: "20px", color: colors.text }}>
+      <p style={{ marginBottom: "20px" }}>
         I have always had a fascination with computers — like many others, this
         fascination sprung from the joy of playing Super Mario Bros on the NES —
         but it was really during my four years at Arizona State University,
         studying as a biology student, that I decided to pursue computer
         programming as a genuine career option.
       </p>
-      <p style={{ color: colors.text }}>
+      <p>
         From the moment I typed out my first line of Python code, to the moment
         I compiled my first C program, to now, my love of software development
         has only grown stronger. I believe there's no limit to what a human with
