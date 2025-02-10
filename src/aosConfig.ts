18e3e8c036771c83c128d83c5530cee9fb4c176f
abarolli <obarolli@gmail.com>
2025-02-10 16:06:41 -0700

    AOS initialized after window load

--- src/aosConfig.ts
+++ src/aosConfig.ts
@@ -3,11 +3,13 @@ import AOS from "aos";
 let isInitialized = false;
 export default function initAOS() {
   if (!isInitialized) {
-    console.log("Initializing AOS");
-    AOS.init({
-      mirror: false,
-      once: true,
-    });
-    isInitialized = true;
+    window.onload = () => {
+      AOS.init({
+        mirror: false,
+        once: true,
+      });
+      isInitialized = true;
+      console.log("AOS initialized");
+    };
   }
 }
